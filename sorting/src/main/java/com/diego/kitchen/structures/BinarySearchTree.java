37a91544e9c3c8afefe1c569af5518331f9adf67
Diego Medina <diego.medina@madefire.com>
2020-12-13 16:16:01 -0800

    Add BST

---
+++ sorting/src/main/java/com/diego/kitchen/structures/BinarySearchTree.java
@@ -0,0 +1,92 @@
+package com.diego.kitchen.structures;
+
+public class BinarySearchTree<T extends Comparable<T>> {
+
+    /**
+     * Internal node structure
+     * @param <T> Any object that extends Comparable. Use to determine the
+     *           location of a node in the tree.
+     */
+    private class Node<T extends Comparable<T>> {
+        public T data;
+        public Node<T> left;
+        public Node<T> right;
+
+        public Node(final T data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+
+        public Node() {
+            this(null);
+        }
+    }
+
+    private Node<T> rootNode;
+    public BinarySearchTree() {
+        rootNode = null;
+    }
+
+    /**
+     * Add a value to the tree
+     * @param value The value to be added.
+     */
+    public void add(final T value) {
+        if (rootNode == null) {
+            rootNode = new Node<>(value);
+        } else {
+            addHelper(rootNode, value);
+        }
+
+    }
+
+    /**
+     * Helper function to add things to the tree.
+     * @param currentNode The current place in the traversal.
+     * @param value The value to be added.
+     * @TODO: Balance the tree when a new node is added.
+     */
+    private void addHelper(Node<T> currentNode, final T value) {
+        if (currentNode == null) {
+            return;
+        }
+
+        // Check where in the tree the item needs to go.
+        final T currentValue = currentNode.data;
+        if (currentValue.compareTo(value) < 0) {
+            // Is greater go to the right of the tree
+            if (currentNode.right == null) {
+                currentNode.right = new Node<>(value);
+            } else {
+                addHelper(currentNode.right, value);
+            }
+        } else {
+            if (currentNode.left == null) {
+                currentNode.left = new Node<>(value);
+            } else {
+                addHelper(currentNode.left, value);
+            }
+        }
+    }
+
+    public String printInOrder() {
+        final StringBuilder stringBuilder = new StringBuilder("[ ");
+        printInOrderHelper(stringBuilder, rootNode);
+        stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length());
+        stringBuilder.append(" ]");
+        return stringBuilder.toString();
+    }
+
+    private void printInOrderHelper(final StringBuilder stringBuilder,  Node<T> currentNode) {
+        if (currentNode == null) {
+            return;
+        }
+
+        stringBuilder.append(currentNode.data.toString());
+        stringBuilder.append(", ");
+
+        printInOrderHelper(stringBuilder, currentNode.left);
+        printInOrderHelper(stringBuilder, currentNode.right);
+    }
+}
---
+++ sorting/src/test/java/com/diego/kitchen/structures/BinarySearchTreeTest.java
@@ -0,0 +1,33 @@
+package com.diego.kitchen.structures;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class BinarySearchTreeTest extends TestCase {
+
+    @Test
+    public void testAdd() {
+        final BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        bst.add(3);
+        bst.add(6);
+        bst.add(1);
+        bst.add(66);
+        bst.add(429);
+
+        assertNotNull(bst);
+    }
+
+    @Test
+    public void testPrintInOrder() {
+        final BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        bst.add(3);
+        bst.add(6);
+        bst.add(1);
+        bst.add(66);
+        bst.add(429);
+
+        final String result = bst.printInOrder();
+        assertEquals("[ 3, 1, 6, 66, 429 ]", result);
+    }
+}
